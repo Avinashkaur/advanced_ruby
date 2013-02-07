0759f85f091c48acf88fafc0b5767d2dba9e0cd9
Avinash kaur <avinash.kaur@vinsol.com>
2013-02-07 13:41:08 +0530

    interactive

---
+++ interactive.rb
@@ -0,0 +1,16 @@
+expr = ""
+loop do
+  $/ = "\n"
+  expr += gets
+  begin 
+    if expr =~ /^[\s]*$\n/ 
+      puts eval(expr)
+      expr = ""
+    end
+    exit(0) if expr =~ /q/i
+  rescue
+    puts "Error!!..Enter again"
+    expr = ""
+    next
+  end
+end
