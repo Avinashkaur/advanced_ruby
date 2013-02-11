744d409a30811d748218bbea698778f5d428696a
Avinash kaur <avinash.kaur@vinsol.com>
2013-02-11 18:43:45 +0530

    revised_code

--- singleton.rb
+++ singleton.rb
@@ -2,17 +2,15 @@ class String
   def say_hello
     puts "Error: Undefined Method!!"
   end
-  class<<self
-    def say_hello
-      puts "Class method for #{self}"
-    end
-  end
 end
-
 name = "av"
 def name.say_hello
   puts "Hello #{self}"
 end
+class<<name
+  def say_hello
+    puts "Class method for #{self}"
+  end
+end
 name.say_hello
 "simran".say_hello #any other instance
-String.say_hello #calling class method
