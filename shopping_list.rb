0278409568d6ae2ab02be60e980afaca48ec8644
Avinash kaur <avinash.kaur@vinsol.com>
2013-02-12 11:34:22 +0530

    shopping_list_new

--- shopping_list.rb
+++ shopping_list.rb
@@ -6,24 +6,24 @@ class ShoppingList
     instance_eval(&block)
   end
   def add(name, quantity)
-    # @list << "#{name} :#{quantity} \n"
     if @list.has_key?(name)
-      @list[name] = quantity
+      @list[name] += quantity
     else
       @list[name] = quantity
     end
   end
   def show
-    puts "#{name}--#{quantity}"
+    @list.each_pair do |key,value|
+      puts "#{key} : #{value}"
+    end  
   end
 end
 s1 = ShoppingList.new
 s1.items do 
   add("toothpaste", 2)
-  add("cold cream", 3)
+  add("shirt", 9)
   add("biscuits", 6)
   add("shirt", 2)
 end
 puts "Your Shopping List-->"
-# puts s1.show
 s1.show
