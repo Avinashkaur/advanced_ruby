756a3b54d406a7a69c4667efba1b11dce2064014
Avinash kaur <avinash.kaur@vinsol.com>
2013-02-25 11:25:23 +0530

    revised_code

--- dynamic_method.rb
+++ dynamic_method.rb
@@ -2,23 +2,21 @@ class NewClass < String
   def initialize(name)
     super(name)
   end
-  def exclude?
-    puts "Enter the value:"
-    value = gets.chomp
-    puts "Is the value not found in the string entered? #{!self.include?(value)} "
+  def exclude?(val)
+    !self.include?(val)
   end
   def to_uppercase
-    puts self.upcase
+    self.upcase
   end
   def no_of_vowels
     count = 0
     self.gsub(/[aeiou]/i) { |character| count += 1 }
-    puts "No. of vowels #{count}"
+    "No. of vowels #{count}"
   end 
 end
 puts "Create the object of the class NewClass in the format: <variable> = NewClass.new('<any-string>')"
 element =  gets
 puts "Select a method to call from the following in the format element.<methodName> :"
-puts "1.exclude? 2.to_uppercase 3.no_of_vowels"
+puts "1.exclude?('<char>') 2.to_uppercase 3.no_of_vowels"
 element += gets
-eval(element)
+puts eval(element)
