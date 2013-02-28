2f45dfa2191715af999e916ebae71c439eb3c00a
Avinash kaur <avinash.kaur@vinsol.com>
2013-02-28 10:35:14 +0530

    refactored-code

--- csv/class_from_csv.rb
+++ csv/class_from_csv.rb
@@ -1,46 +1,28 @@
-$method_array_name = []
 $object_array = []
-def get_methods(filename)
-  File.open(filename) do |file|  
-    file.each_line.first.chomp
-  end
-end
-def create_new_class(klass, filename)
-  method_array = get_methods(filename)
-  method_array.split(",").each do |method_name|
-    $method_array_name << method_name
-  end
+def create_new_class(filename)
+  klass = filename.split(".").first.capitalize
+  $FILEREADER = File.new(filename)
+  $method_array_names = ($FILEREADER.gets.chomp!).split(',')
   Object::const_set(klass.to_sym, Class.new do
-    $method_array_name.each do |element|
-      attr_accessor element.to_sym
-    end
+    $method_array_names.each { |element| attr_accessor element.to_sym }
   end)
 end
-def create_object_array(filename, klass)
-  File.open(filename, "r") do |file|  
-    file.each_line do |line|
-      method_names = get_methods(filename)
-      if !(line.match(method_names))
-        object = klass.new
-        records = line.split(",").each { |element| element.strip! }
-        for i in 0..($method_array_name.size-1)
-          object.send "#{$method_array_name[i]}=", records[i]
-        end
-        $object_array << object
-      end
+def create_object_array( klass )  
+  while line = $FILEREADER.gets do 
+    object = klass.new
+    records = line.split(",").each { |element| element.strip! }
+    ($method_array_names.size).times do |i|
+      object.send "#{$method_array_names[i]}=", records[i]
     end
+    $object_array << object
   end
+  display_object_array(klass)
 end
 def display_object_array(klass)
   $object_array.each do |object|
-    $method_array_name.each do |meth|
-      puts "#{object.send meth}"
-    end
+    $method_array_names.each { |meth| puts "#{object.send meth}" }
   end
 end
 puts "enter file name : trial.csv or emp.csv or emp1.csv"
-name = gets.chomp
-classname = name.to_s.split(".").first.capitalize
-klass = create_new_class(classname, name)
-create_object_array(name, klass)
-display_object_array(klass)
+klass = create_new_class(gets.chomp)
+create_object_array(klass)
--- object_store.rb
+++ object_store.rb
@@ -1,46 +1,60 @@
 $object_array = []
-
+ARGS = []
 module ObjectStore
+  
   module ClassMethods
+    
     def validate_presence_of(*args)
-      args_array = args
-      args_array.each do |element|
-        if (element.size == 0)
-          exit(0)
-        end
-      end
+      puts "called with #{args}"
+      ARGS << args
+      # .each do |element|
+      #   puts "in presence func"
+      #   puts "#{element}" 
+      #   puts instance_variable_get("@#{element}")
+      # end
     end
+    
     def create_method(args_array)
       ghostclass = class<<self; self; end
       ghostclass.instance_eval do
         args_array.each do |arg|
           define_method("find_by_#{arg}") do |value|
-            $object_array.each do |object|
-              if (object.instance_variable_get("@#{arg}") == value)
-                p "Found #{value}"
-                puts "First Name: #{object.instance_variable_get("@fname")}"
-                puts "Age: #{object.instance_variable_get("@age")}"
-                puts "Email: #{object.instance_variable_get("@email")}"
-              end
+            arr = $object_array.select do |object|
+              "#{object.fname}, #{object.age}, #{object.email}" if (object.instance_variable_get("@#{arg}") == value)
             end
+            arr
           end
         end
       end
     end
+    def collect
+      puts "Objects: "
+      $object_array.each { |obj| puts obj }
+    end
+    def count
+      puts "Total objects: #{$object_array.size}"
+    end
   end
   def save
-    begin
-      if validate 
-        $object_array << self
-        arr = []
-        self.instance_variables.each { |ele| arr << ele.to_s.gsub!(/\@/, "")}
-        Play.create_method(arr)
-      else
-        puts "This object cannot be inserted into the array!!"
-      end
-    rescue
-      p "Module cannot be included!!"
-    end
+    var_arr = self.instance_variables.select { |ele| ele.to_s.gsub!(/\@/, "") }
+    puts "value is #{ARGS.flatten!}"
+    # puts "#{ARGS}"
+    puts "var_arr: #{var_arr}"
+    puts "#{ARGS - var_arr}"
+    # if (ARGS - var_arr).empty?
+    #   begin
+    #     if !validate 
+    #       puts "This object cannot be inserted into the array!!"
+    #       exit 0
+    #     end
+    #   rescue
+    #     puts "no validation done"
+    #   ensure
+    #     $object_array << self
+    #     arr = instance_variables.select { |ele| ele.to_s.gsub!(/\@/, "")}
+    #     Play.create_method(arr)
+    #   end
+    # end
   end
   def self.included(klass)
     klass.extend ClassMethods
@@ -62,12 +76,32 @@ p2.fname = "av"
 p2.age = 23
 p2.email = "avinash.kaur@vinsol.com"
 p2.save
+puts $object_array
+# p3 = Play.new
+# p3.fname = "avinash"
+# p3.age = 19
+# p3.email = "avi.kaur9@gmail.com"
+# p3.save
 
-p3 = Play.new
-p3.fname = "avinash"
-p3.age = 19
-p3.email = "avi.kaur9@gmail.com"
-p3.save
-Play.find_by_age(23)
-Play.find_by_fname("avinash")
-Play.find_by_email("avinash.kaur@vinsol.com")
+# p4 = Play.new
+# p4.fname = "simran"
+# p4.age = 23
+# p4.email = "sim@gmail.com"
+# p4.save
+# puts Play.find_by_age(23)
+# puts Play.find_by_fname("avinash")
+# puts Play.find_by_email("avinash.kaur@vinsol.com")
+# Play.collect
+# Play.count
+
+# class Clay
+#   include ObjectStore
+#   attr_accessor :age, :fname, :email
+#   validate_presence_of :fname, :age 
+# end
+
+# c = Clay.new
+# c.fname = "paris"
+# c.age = 26
+# c.email = "paris.hilton@gmail.com"
+# c.save
